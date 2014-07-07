2d1e180db1087504f68da2af8340d07344568994
Donald Valverde <cavious@gotbawlz.com>
2014-07-07 08:43:25 -0400

    Main Python File

---
+++ main.py
@@ -0,0 +1,34 @@
+
+import pygame, sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 360), 0, 32)
+
+clock = pygame.time.Clock()
+FPS = 24
+
+clr1 = (22, 122, 211)
+clr2 = (0, 44, 166)
+clr3 = (34, 55, 245)
+
+while True:
+    #PROCESSES
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #PROCESSES
+    #LOGIC
+
+    #LOGIC
+    #DRAW
+
+    pygame.draw.line(screen, clr2, (0, 0),(640, 360))
+
+    pygame.display.flip()
+    #DRAW
+
+    clock.tick(FPS)
+    print(clock)
